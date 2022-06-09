342b34d0a443fffd5077ea9523e0508c42595aaa
petrucior <petrucior@MacBook-Air-de-petrucior.local>
2022-06-09 14:22:32 -0300

    updating pwm.c

--- lecture4/pwm.c
+++ lecture4/pwm.c
@@ -19,15 +19,15 @@
 
 int main( void ){
   
-  // Set all D pins registers as output ( DDRxn --> DDRx )
-                      //        7654_3210
-  DDRD |= 0xFF;       // binary xxxx_x1xx
+  // Set pin PD6 and PD5 registers as output ( DDRxn --> DDRx )
+                                     //        7654_3210
+  DDRD |= (1 << 6) | (1 << 5);       // binary x11x_xxxx
   
   // Enabling Fast PWM mode, TOP = 0xFF, and OC0A e OC0B
   TCCR0A = 0b10100011; // PWM not inverted in OC0A and OC0B 
   TCCR0B = 0b00000011; // Turn on TC0, prescaler = 64, fpwm = fosc / (2^{x} * prescaler) = 16Mhz / ( 256 * 64 ) = 976 Hz
-  OCR0A = 240;         // PWM active cycle control (OC0A - PD6)
-  //OCR0B = 50;          // PWM active cycle control (OC0B - PD5)
+  OCR0A = 200;         // PWM active cycle control (OC0A - PD6) --> duty cycle = 200/256 = 78.13%
+  //OCR0B = 50;          // PWM active cycle control (OC0B - PD5) --> duty cycle = 50/256 = 19.53%
   
   // Infinity loop
   while ( 1 ){;;}

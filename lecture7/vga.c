80748ea9dae051ea58d0a246967a6b7d607d8e6d
petrucior <petrucior@gmail.com>
2023-10-18 13:39:11 -0300

    update vga

--- lecture7/vga.c
+++ lecture7/vga.c
@@ -1,7 +1,7 @@
 /**
- * \file eeprom.c 
+ * \file vga.c 
  *
- * \brief This file saves data in the eeprom using serial comunication (usart)
+ * \brief This file shows an image using vga
  *
  * \author 
  * Petrucio Ricardo Tavares de Medeiros \n
@@ -10,7 +10,7 @@
  * petrucior at gmail (dot) com
  * 
  * \version 0.1
- * \date Jul 2022
+ * \date Oct 2023
  */
 #define F_CPU 16000000UL // Defining clock
 #define BAUD 9600 // Defining Baud Rate
@@ -18,51 +18,110 @@
 #include <avr/io.h> // Including library avr
 #include <avr/interrupt.h> // Including library interrupt
 #include <util/delay.h> // Including library delay
-#include <avr/eeprom.h> // Including library eeprom
 
-// Setting USART
-void usartInit( unsigned int ubrr ){
-  // Adjusting baud rate
-  UBRR0H = (unsigned char)(ubrr >> 8);
-  UBRR0L = (unsigned char)(ubrr);
-  // Enabling interrupt, transmitter and receiver
-  UCSR0B = (1 << RXCIE0) | (1 << RXEN0) | (1 << TXEN0);
-  // Adjusting format frame ( data (8 bits) and 2 stops )
-  UCSR0C = (1 << USBS0) | (3 << UCSZ00);
-}
+// Define os pinos
+#define pin_vsync   PB2
+#define pin_hsync   PD3
+#define pin_red     PD5
+#define pin_green   PD6
+#define pin_blue    PD7
 
-// Sending frame
-void usartSend( unsigned char data ){
-  // Waiting for transmission register to clear
-  while ( !( UCSR0A & (1 << UDRE0 ) ) );
-  UDR0 = data;
-}
+// Comandos para facilitar a manipulação dos registradores
+#define SET_BIT(PORT, PIN) PORT |= _BV(PIN)
+#define CLR_BIT(PORT, PIN) PORT &= ~(_BV(PIN))
 
-// Receive frame
-unsigned char usartReceive( void ){
-  // Waiting for data to be received
-  while( !( UCSR0A & (1 << RXC0 ) ) );
-  return UDR0;
-}
+#define NOP asm ("nop\n\t")
 
-// USART Interrupt
-ISR( USART_RX_vect ){
-  // Sending the received data
-  unsigned char dado = usartReceive();
-  usartSend( dado );
-  eeprom_write_byte( 0, dado );
+// Parametros do padrão VGA
+#define VGA_ALTURA              480
+#define VGA_BACK_PORCH_VERTICAL 35
+
+
+uint8_t video_exibido = 0;
+volatile char back_porch_vertical_restante;
+volatile int linha_atual;
+
+// Delay com a instrução de não operação do microcontrolador                                                                                                                                                
+void delay_nop(uint16_t tempo){
+  for(uint16_t i=0; i<tempo; i++){
+    NOP;
+  }
 }
 
 int main( void ){
+  // Define os pinos como saída
+  SET_BIT(DDRB, pin_vsync);
+  SET_BIT(DDRD, pin_hsync);
+  SET_BIT(DDRD, pin_red);
+  SET_BIT(DDRD, pin_green);
+  SET_BIT(DDRD, pin_blue);
 
-  // Setting usart
-  usartInit( _UBRR );
+  // Desabilita o clock do timer0
+  TCCR0B = 0;
 
-  // Enabling interrupt
-  sei();
-  
-  // Infinity loop
-  while ( 1 ){ ;; }
-  
+  // Configura o timer 1 com prescaler de 1024, no modo fast PWM
+  TCCR1A = _BV(WGM10) | _BV(WGM11) | _BV(COM1B1);
+  TCCR1B = _BV(WGM12) | _BV(WGM13) | _BV(CS12) | _BV(CS10);
+  OCR1A = 259; // Período de 16,64‬ms
+  OCR1B = 0; // Largura de 64us (2 linhas)
+  TIMSK1 = _BV(TOIE1); // Habilita interrupção por estouro
+
+  // Configura o timer 2 com prescaler de 8, no modo fast PWM
+  TCCR2A = _BV(WGM20) | _BV(WGM21) | _BV(COM2B1);
+  TCCR2B = _BV(WGM22) | _BV(CS21);
+  OCR2A = 63; // Período de 32us
+  OCR2B = 7; // Largura de 4us (~96 pixels)
+  TIMSK2 = _BV(TOIE2); // Habilita interrupção por estouro
+
+  sei(); // Habilita as interrupções
+
+
+  while(1) {
+    // Se está na região do vídeo exibido
+    if (video_exibido) {
+      video_exibido = 0;
+      
+      // Desenha a imagem aqui
+      if (linha_atual < VGA_ALTURA/3){
+	SET_BIT(PORTD, pin_green);
+      }
+      else if (linha_atual < 2*VGA_ALTURA/3){
+	SET_BIT(PORTD, pin_red);
+      }
+      else {
+	SET_BIT(PORTD, pin_blue);
+      }
+      delay_nop(80); // Valor empirico para atrasar até o final da linha
+    }
+    // Após desenhar, limpa os canais de cores
+    CLR_BIT(PORTD, pin_red);
+    CLR_BIT(PORTD, pin_green);
+    CLR_BIT(PORTD, pin_blue);
+  }
   return 0;
 }
+
+
+// Interrupção que ocorre a cada inicio de tela
+ISR(TIMER1_OVF_vect){
+  // Reseta a posição da linha e do back porch vertical restante
+  linha_atual = 0;
+  back_porch_vertical_restante = VGA_BACK_PORCH_VERTICAL;
+}
+
+// Interrupção que ocorre a cada inicio de linha
+ISR(TIMER2_OVF_vect){
+  // Subtrai do back porch restante se ele for maior que 0
+  if (back_porch_vertical_restante > 0) {
+    back_porch_vertical_restante--;
+  }
+  // Caso contrário, soma a linha atual
+  else{
+    linha_atual++;
+    // Se a linha atual estiver antes do front porch vertical, seta o video_exibido
+    if (linha_atual < VGA_ALTURA){
+      video_exibido = 1;
+    }
+  }
+}
+ 
